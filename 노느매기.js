a834e60066edc6887cf9e984e37450d4452318db
shsrrk <ksh040903@naver.com>
2023-05-25 13:30:36 +0900

    20230525 1페이지

---
+++ 노느매기.js
@@ -0,0 +1,7 @@
+document.getElementById("myButton").addEventListener("click", function() {
+    // Check the condition
+    if (/* your condition here */) {
+      // Apply the animation to the div element
+      document.getElementById("myDiv").style.animation = "myAnimation 1s";
+    }
+  });

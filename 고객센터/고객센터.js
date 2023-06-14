70ea8e8a3ee8a22ce70bb6aa9050bbcdcfa493eb
shsrrk <ksh040903@naver.com>
2023-06-14 11:42:48 +0900

    고객센터 안 올라갔네 아래랑 같음

--- 고객센터/고객센터.js
+++ 고객센터/고객센터.js
@@ -1,6 +1,42 @@
 $(document).ready(function() {
-    $('.cheating-box::before, .bug-box::before, .csc-box::before').click(function() {
-      $(this).parent().toggleClass('active');
+
+    $(".bug-tag").click(function(next){
+      $(".inquiry-tag").addClass(".inquiry-tag").css('z-index','4');
+      $(".inquiry").addClass(".inquiry").css('display','none');
+
+      $(".cheating-tag").addClass(".cheating-tag").css('z-index','3');
+      $(".cheating").addClass(".cheating").css('display','none');
+
+      $(this).addClass(".bug-tag").css('z-index','5');
+      $(".bug").addClass(".bug").css('display','block');
+      next();
+
     });
-  });
+
+    $(".inquiry-tag").click(function(next){
+      $(".bug-tag").addClass(".bug-tag").css('z-index','4');
+      $(".bug").addClass(".bug").css('display','none');
+
+      $(".cheating-tag").addClass(".cheating-tag").css('z-index','3');
+      $(".cheating").addClass(".cheating").css('display','none');
+
+      $(this).addClass(".inquiry-tag").css('z-index','5');
+      $(".inquiry").addClass(".inquiry").css('display','block');
+      next();
+    });
+
+    $(".cheating-tag").click(function(next){
+      $(".inquiry-tag").addClass(".inquiry-tag").css('z-index','4');
+      $(".inquiry").addClass(".inquiry").css('display','none');
+
+      $(".bug-tag").addClass(".bug-tag").css('z-index','3');
+      $(".bug").addClass(".bug").css('display','none');
+
+      $(this).addClass(".cheating-tag").css('z-index','5');
+      $(".cheating").addClass(".cheating").css('display','block');
+
+      next();
+    });
+
+});
   
